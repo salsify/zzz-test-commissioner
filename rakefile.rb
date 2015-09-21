980ba33e359a10c44a8e3c3582d1cc696a61b683
pietdaniel <dpiet@salsify.com>
2015-09-21 16:33:15 -0400

    proper test configs

--- rakefile.rb
+++ rakefile.rb
@@ -1,2 +1,8 @@
 require_relative './app/app.rb'
 require 'sinatra/activerecord/rake'
+
+task :environment do
+    Sinatra::Application.environment = ENV['RACK_ENV']
+end
+
+Dir.glob('lib/tasks/*.rake').each { |r| load r}
