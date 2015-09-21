972706f52e96f28a51d7344a30b035234a6761ac
pietdaniel <dpiet@salsify.com>
2015-09-21 16:12:39 -0400

    uncomment password

--- app/database.rb
+++ app/database.rb
@@ -6,7 +6,7 @@ ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
   :username => db.user,
-  #:password => db.password,
+  :password => db.password,
   :database => db.path[1..-1],
   :encoding => 'utf8'
 )
